7be436ca7c317d052c82e50f950d7c7e734cfa01
kris <kris@viirose.com>
2019-03-22 18:42:19 +0800

    init
    init

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Wechat\Pub;
+use Input;
 
 class WechatController extends Controller
 {
--- app/Wechat/Pub.php
+++ app/Wechat/Pub.php
@@ -3,7 +3,7 @@
 namespace App\Wechat;
 
 use Cache;
-use Input;
+// use Input;
 use Exception;
 
 /**
