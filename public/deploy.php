50ad8983acd6296284ca4b41ced4d0be6f9e5089
kris <kris@viirose.com>
2019-03-22 13:52:03 +0800

    init
    init

--- public/deploy.php
+++ public/deploy.php
@@ -1,28 +1,16 @@
 <?php
-
-// $work_path = getcwd();
-// $path = str_replace("public","",$work_path);
-
-$path = '/mnt/www/zl.viirose.com/';
-
-$key = 'kingking';
-
-$github_signature = @$_SERVER['HTTP_X_HUB_SIGNATURE'];
-$payload = file_get_contents('php://input');
-
-$arr = explode('=', $github_signature);
-$algo = $arr[0];
-$signature = $arr[1];
-
-$payload_hash = hash_hmac($algo, $payload, $key);
-if($payload_hash == $signature){
-    shell_exec('cd '.$path);
-    shell_exec('/usr/bin/git pull');
-    // shell_exec('/usr/bin/composer install --optimize-autoloader');
-    // shell_exec('/usr/bin/php artisan config:cache');
-    // shell_exec('/usr/bin/php artisan route:cache');
-    shell_exec('chown -R nginx:nginx'.$path);
-    return 200;
-}else{
-   return 'invalid key!'; 
-} 
+// GitHub Webhook Secret.
+// Keep it the same with the 'Secret' field on your Webhooks / Manage webhook page of your respostory.
+$secret = "kingking";
+// 项目根目录, 如: "/var/www/fizzday"
+$path = "/mnt/www/zl.viirose.com";
+// Headers deliveried from GitHub
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+if ($signature) {
+    $hash = "sha1=" . hash_hmac('sha1', $HTTP_RAW_POST_DATA, $secret);
+    if (strcmp($signature, $hash) == 0) {
+        echo shell_exec("cd {$path} && /usr/bin/git reset --hard origin/master && /usr/bin/git clean -f && /usr/bin/git pull 2>&1");
+        exit();
+    }
+}
+http_response_code(404);
