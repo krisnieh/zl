a356dc341b5856a2057feeb94dcad0daa253f8db
kris <kris@viirose.com>
2019-03-23 13:33:39 +0800

    init
    init

--- app/Wechat/Templets.php
+++ app/Wechat/Templets.php
@@ -80,23 +80,7 @@ class Templets
               </Articles>
             </xml>
         ';
-
-        $orign = '<xml>
-<ToUserName><![CDATA['.$array['OpenID'].']]></ToUserName>
-  <FromUserName><![CDATA['.$this->pub->name.']]></FromUserName>
-  <CreateTime>'.time().'</CreateTime>
-  <MsgType><![CDATA[news]]></MsgType>
-  <ArticleCount>1</ArticleCount>
-  <Articles>
-    <item>
-      <Title><![CDATA[title1]]></Title>
-      <Description><![CDATA[description1]]></Description>
-      <PicUrl><![CDATA[https://zl.viirose.com/image/welcome.jpg]]></PicUrl>
-      <Url><![CDATA[https://zl.viirose.com]]></Url>
-    </item>
-  </Articles>
-</xml>';
-        $out = preg_replace('/\s(?=)/', '', sprintf($orign));
+        $out = preg_replace('/\s(?=)/', '', sprintf($templet));
         return $out;
     }
 
