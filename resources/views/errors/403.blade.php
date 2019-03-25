8404dfe57ca94652d01ae1f80aaa4db1f1afdbbd
kris <kris@viirose.com>
2019-03-25 13:06:21 +0800

    init
    init

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="d-flex justify-content-center align-items-center w-100 alert">
     <div class="card bg-warning text-white col-10 col-sm-5">
-        <div class="card-body"><h5>403</h5><p>权限不足, 或账号异常</p></div>
+        <div class="card-body"><h5>权限不足</h5><p>可能是账号正在审核或被冻结</p></div>
     </div>
 </div>
 
