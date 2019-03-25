f9dd0b99e0372b77476b861eca653c8e132cf55a
kris <kris@viirose.com>
2019-03-25 09:21:04 +0800

    init
    init

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -6,10 +6,7 @@
         <div class="card-body">
             <img class="ok" src="{{ URL::asset('svg/logo.svg') }}">
             <h5>众乐速配</h5>
-           <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->generate($url)) !!} ">
-            <p>
-                
-            </p>
+           <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->merge('image/qrcode_logo.png', .2)->generate($url)) !!} ">
         </div>
     </div>
 </div>
