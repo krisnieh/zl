14bf769e0e406f1662f086600cc5f5893d8a1a2e
kris <kris@viirose.com>
2019-03-25 10:23:14 +0800

    init
    init

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="d-flex justify-content-center align-items-center w-100 alert">
     <div class="card bg-default col-12 col-sm-6">
-           <img class="img-fluid" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('/public/image/qrcode_logo.png', .2)->errorCorrection('H')->size(220)->generate($url)) !!} ">
+           <img class="img-fluid" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('/public/image/qrcode_logo.png', .2)->size(220)->generate($url)) !!} ">
             <small class="text text-grey">欢迎关注众乐速配</small>
         </div>
     </div>
