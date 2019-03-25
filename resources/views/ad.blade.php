a6388ab7a826c3ae184f532cf5196f5eabfff5bf
kris <kris@viirose.com>
2019-03-25 10:18:23 +0800

    init
    init

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -3,9 +3,8 @@
 @section('content')
 <div class="d-flex justify-content-center align-items-center w-100 alert">
     <div class="card bg-default col-12 col-sm-6">
-           <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->merge('/public/image/qrcode_logo.png', .2)->errorCorrection('H')->generate($url)) !!} ">
-            <h5>众乐速配</h5>
-            <small class="text text-grey">提示: 有效1天</small>
+           <img class="rounded" src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(200)->merge('/public/image/qrcode_logo.png', .2)->errorCorrection('H')->generate($url)) !!} ">
+            <small class="text text-grey">欢迎关注众乐速配</small>
         </div>
     </div>
 </div>
