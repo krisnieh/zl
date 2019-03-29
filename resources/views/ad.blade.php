645fe88cab471299e88295d5494883e33c531d27
kris <kris@viirose.com>
2019-03-29 12:50:47 +0800

    init
    init

--- resources/views/ad.blade.php
+++ resources/views/ad.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="d-flex justify-content-center align-items-center w-100 alert">
     <div class="card bg-default col-12 col-sm-4 align-items-center">
+        <br>
         @if(count($qrcode))
            <div class="row"><img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->merge('/public/image/qrcode_logo.png', .2)->errorCorrection('H')->size(200)->generate($qrcode['url'])) !!} "></div>
             <div class="row"><small class="text text-danger">{{ date('Y-m-d  h:i:s', $qrcode['expire']) }} 后过期</small></div>
