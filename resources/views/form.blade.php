d97736bdaf781e0900bcf313f47da72d23c634d5
kris <kris@viirose.com>
2019-04-05 19:26:09 +0800

    init
    init

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,7 @@
 <div class="d-flex justify-content-center align-items-center w-100">
 <div class="card bg-light text-dark col-sm-4">
     <br>
-    <h6><i class="fa fa-{{ $icon }} ico-space" aria-hidden="true"></i>{{ $title }}</h6>
+    <h6><i class="fa fa-{{ $icon }} ico-space" aria-hidden="true"></i>{!! $title !!}</h6>
     <div class="card-body">
         {!! form($form) !!}
     </div>
