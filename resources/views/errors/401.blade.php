d667223e6410568aae0fbbf663d04d5e49150bcc
kris <kris@viirose.com>
2019-04-06 05:46:32 +0800

    init
    init

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -4,6 +4,7 @@
 <div class="d-flex justify-content-center align-items-center w-100 alert">
     <div class="alert alert-warning col-10 col-sm-5">
         <h1><i class="fa fa-calendar-times-o" aria-hidden="true"></i></h1><p>已过期</p>
+        <p><a href="javascript:history.back(-1)" class="btn btn-warning btn-sm btn-block">返回</a></p>
     </div>
 </div>
 
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -4,6 +4,7 @@
 <div class="d-flex justify-content-center align-items-center w-100 alert">
     <div class="alert alert-danger col-10 col-sm-5">
         <h1><i class="fa fa-fire" aria-hidden="true"></i></h1><p>页面或者服务不存在</p>
+        <p><a href="javascript:history.back(-1)" class="btn btn-warning btn-sm btn-block">返回</a></p>
     </div>
 </div>
 
