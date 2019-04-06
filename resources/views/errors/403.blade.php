8786fd6ab0de45204104161b1783b3ffcddfe501
kris <kris@viirose.com>
2019-04-06 08:29:50 +0800

    init
    init

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -5,7 +5,7 @@
     <div class="alert alert-warning col-10 col-sm-5">
         <h1><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></h1><p>权限不足</p>
     </div>
-    <p><a href="javascript:history.back(-1)" class="btn btn-info">返回</a></p>
+    <div class="col-10 col-sm-5"><a href="javascript:history.back(-1)" class="btn btn-info btn-block">返回</a></div>
 </div>
 
 @endsection
