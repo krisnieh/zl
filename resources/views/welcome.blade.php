0db140aaf5bdf88b5aa64a2e7fcfc912f68707e1
kris <kris@viirose.com>
2019-04-09 22:18:45 +0800

    init
    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,16 @@
 
 @section('content')
 
-    <div class="card bg-default col-12 col-sm-6 cent">
-        <div class="card-body">
+    <div class="col-12 col-sm-6 cent">
+        <div>
             <img class="ok" src="{{ URL::asset('svg/logo.svg') }}">
             <h5>众乐速配</h5>
-            <p class="text text-danger">省钱 . 高效</p>
+            <p class="text text-danger">高能 . 高效</p>
             <p>
                 @if(strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') && Cache::has(session('openid')))
                     <a href="/register" class="btn btn-block btn-success">继续, 完成注册</a>
                 @else
-                    <a href="/apps" class="btn-success btn btn-block">前往应用中心</a>
+                    <a href="/apps" class="btn-success btn btn-success btn-sm"><i class="fa fa-th ico-space" aria-hidden="true"></i> 前往应用中心</a>
                 @endif
             </p>
         </div>
