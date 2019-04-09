c4b0cf3e23b4be7cc354057ba68c224e2e7da9d6
kris <kris@viirose.com>
2019-04-09 13:50:33 +0800

    init
    init

--- database/seeds/OrgsTableSeeder.php
+++ database/seeds/OrgsTableSeeder.php
@@ -36,53 +36,53 @@ class OrgsTableSeeder extends Seeder
             'auth' => '{"root":true}'
         ]);
 
-        Org::create([
-            'name' => '常州速配',
-            'conf_id' => 5,
-            'parent_id' => 3,
-            'info' => '{"province":"江苏", "city":"无锡", "sub_city":"常州"}',
-            'auth' => '{"root":true}'
-        ]);
+        // Org::create([
+        //     'name' => '常州速配',
+        //     'conf_id' => 5,
+        //     'parent_id' => 3,
+        //     'info' => '{"province":"江苏", "city":"无锡", "sub_city":"常州"}',
+        //     'auth' => '{"root":true}'
+        // ]);
 
-        Org::create([
-            'name' => '湖兴',
-            'conf_id' => 6,
-            'parent_id' => 3,
-            'info' => '{"province":"江苏", "city":"常州", "sub_city":"溧阳"}',
-            'auth' => null
-        ]);
+        // Org::create([
+        //     'name' => '湖兴',
+        //     'conf_id' => 6,
+        //     'parent_id' => 3,
+        //     'info' => '{"province":"江苏", "city":"常州", "sub_city":"溧阳"}',
+        //     'auth' => null
+        // ]);
 
-        Org::create([
-            'name' => '锡城特快',
-            'conf_id' => 6,
-            'parent_id' => 3,
-            'info' => '{"province":"江苏", "city":"无锡", "sub_city":"无锡"}',
-            'auth' => null
-        ]);
+        // Org::create([
+        //     'name' => '锡城特快',
+        //     'conf_id' => 6,
+        //     'parent_id' => 3,
+        //     'info' => '{"province":"江苏", "city":"无锡", "sub_city":"无锡"}',
+        //     'auth' => null
+        // ]);
 
-        Org::create([
-            'name' => '江阴飞龙',
-            'conf_id' => 6,
-            'parent_id' => 3,
-            'info' => '{"province":"江苏", "city":"无锡", "sub_city":"江阴"}',
-            'auth' => null
-        ]);
+        // Org::create([
+        //     'name' => '江阴飞龙',
+        //     'conf_id' => 6,
+        //     'parent_id' => 3,
+        //     'info' => '{"province":"江苏", "city":"无锡", "sub_city":"江阴"}',
+        //     'auth' => null
+        // ]);
 
-        Org::create([
-            'name' => '宜兴中天',
-            'conf_id' => 6,
-            'parent_id' => 3,
-            'info' => '{"province":"江苏", "city":"无锡", "sub_city":"宜兴"}',
-            'auth' => null
-        ]);
+        // Org::create([
+        //     'name' => '宜兴中天',
+        //     'conf_id' => 6,
+        //     'parent_id' => 3,
+        //     'info' => '{"province":"江苏", "city":"无锡", "sub_city":"宜兴"}',
+        //     'auth' => null
+        // ]);
 
-        Org::create([
-            'name' => '新桥修理店',
-            'conf_id' => 7,
-            'parent_id' => 7,
-            'info' => '{"addr":"江阴新桥镇胡叶街128号"}',
-            'auth' => null
-        ]);
+        // Org::create([
+        //     'name' => '新桥修理店',
+        //     'conf_id' => 7,
+        //     'parent_id' => 7,
+        //     'info' => '{"addr":"江阴新桥镇胡叶街128号"}',
+        //     'auth' => null
+        // ]);
 
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -30,59 +30,59 @@ class UsersTableSeeder extends Seeder
             'auth' => '{"root":true}'
         ]);
 
-        User::create([
-            'parent_id' => 2,
-            'org_id' => 3,
-            'accounts' =>'{"mobile":"13000000000", "openid":"wechatopenid"}',
-            'password' => bcrypt('000000'),
-            'info' => '{"name":"陈东阳", "addr":"无锡江阴青阳"}',
-            'auth' => '{"admin":true}'
-        ]);
+        // User::create([
+        //     'parent_id' => 2,
+        //     'org_id' => 3,
+        //     'accounts' =>'{"mobile":"13000000000", "openid":"wechatopenid"}',
+        //     'password' => bcrypt('000000'),
+        //     'info' => '{"name":"陈东阳", "addr":"无锡江阴青阳"}',
+        //     'auth' => '{"admin":true}'
+        // ]);
 
-        User::create([
-            'parent_id' => 2,
-            'org_id' => 3,
-            'accounts' =>'{"mobile":"13111111111", "openid":"wechatopenid"}',
-            'password' => bcrypt('000000'),
-            'info' => '{"name":"周芷若", "addr":"无锡江阴青阳"}',
-            'auth' => '{"master":true}'
-        ]);
+        // User::create([
+        //     'parent_id' => 2,
+        //     'org_id' => 3,
+        //     'accounts' =>'{"mobile":"13111111111", "openid":"wechatopenid"}',
+        //     'password' => bcrypt('000000'),
+        //     'info' => '{"name":"周芷若", "addr":"无锡江阴青阳"}',
+        //     'auth' => '{"master":true}'
+        // ]);
 
-        User::create([
-            'parent_id' => 2,
-            'org_id' => 3,
-            'accounts' =>'{"mobile":"13222222222", "openid":"wechatopenid"}',
-            'password' => bcrypt('000000'),
-            'info' => '{"name":"丁敏君", "addr":"无锡江阴青阳"}',
-            'auth' => null
-        ]);
+        // User::create([
+        //     'parent_id' => 2,
+        //     'org_id' => 3,
+        //     'accounts' =>'{"mobile":"13222222222", "openid":"wechatopenid"}',
+        //     'password' => bcrypt('000000'),
+        //     'info' => '{"name":"丁敏君", "addr":"无锡江阴青阳"}',
+        //     'auth' => null
+        // ]);
 
-        User::create([
-            'parent_id' => 2,
-            'org_id' => 7,
-            'accounts' =>'{"mobile":"13444444444", "openid":"wechatopenid"}',
-            'password' => bcrypt('000000'),
-            'info' => '{"name":"令狐冲", "addr":"无锡江阴青阳"}',
-            'auth' => '{"master":true}'
-        ]);
+        // User::create([
+        //     'parent_id' => 2,
+        //     'org_id' => 7,
+        //     'accounts' =>'{"mobile":"13444444444", "openid":"wechatopenid"}',
+        //     'password' => bcrypt('000000'),
+        //     'info' => '{"name":"令狐冲", "addr":"无锡江阴青阳"}',
+        //     'auth' => '{"master":true}'
+        // ]);
 
-        User::create([
-            'parent_id' => 2,
-            'org_id' => 7,
-            'accounts' =>'{"mobile":"13555555555", "openid":"wechatopenid"}',
-            'password' => bcrypt('000000'),
-            'info' => '{"name":"任盈盈", "addr":"fexxxxxxyyyy"}',
-            'auth' => '{"master":true}'
-        ]);
+        // User::create([
+        //     'parent_id' => 2,
+        //     'org_id' => 7,
+        //     'accounts' =>'{"mobile":"13555555555", "openid":"wechatopenid"}',
+        //     'password' => bcrypt('000000'),
+        //     'info' => '{"name":"任盈盈", "addr":"fexxxxxxyyyy"}',
+        //     'auth' => '{"master":true}'
+        // ]);
 
-        User::create([
-            'parent_id' => 2,
-            'org_id' => 9,
-            'accounts' =>'{"mobile":"13666666666", "openid":"wechatopenid"}',
-            'password' => bcrypt('000000'),
-            'info' => '{"name":"莫大先生", "addr":"无锡江阴新桥"}',
-            'auth' => '{"master":true}'
-        ]);
+        // User::create([
+        //     'parent_id' => 2,
+        //     'org_id' => 9,
+        //     'accounts' =>'{"mobile":"13666666666", "openid":"wechatopenid"}',
+        //     'password' => bcrypt('000000'),
+        //     'info' => '{"name":"莫大先生", "addr":"无锡江阴新桥"}',
+        //     'auth' => '{"master":true}'
+        // ]);
 
         
     }
