8a1e5c51545a323cd731202ff573bd89e955df71
kris <kris@viirose.com>
2019-04-09 19:52:29 +0800

    init
    init

--- resources/views/finance.blade.php
+++ resources/views/finance.blade.php
@@ -6,18 +6,21 @@
 @section('content')
 <nav class="breadcrumb">
     <a class="breadcrumb-item text-dark" href="/apps"><i class="fa fa-th ico-space" aria-hidden="true"></i>应用</a>
-    <span class="breadcrumb-item active"><i class="fa fa-jpy ico-space" aria-hidden="true"></i>充值</span>
+    <span class="breadcrumb-item active"><i class="fa fa-jpy ico-space" aria-hidden="true"></i>财务</span>
 </nav>
     @if(isset($records) && count($records))
         @foreach($records as $record)
             <div class="card card-list">
                 <div class="card-header card-h"><small>#</small> <strong>{{ $record->id }}</strong><small class="pull-right">{{ $record->created_at->diffForHumans() }} {{ $record->created_at }}</small> </div>
                 <div class="card-body">
-                    <p class="thin-p"><span class="badge badge-{{ $record->add ? 'success' : 'danger' }}">{{ $record->from->name }} 充值 ¥{{ $record->pay }}</span> 
+                    <p class="thin-p"><span class="badge badge-{{ $record->add ? 'success' : 'danger' }}">{{ $record->from->name }} {{ $record->add ? '充值' : '消费' }} ¥{{ $record->pay }}</span> 
                         @if($r->own($record->from_user) && $record->state == 0)
                         <a class="badge badge-danger" href="/finance/delete/{{ $record->id }}"><i class="fa fa-fire" aria-hidden="true"></i> 删除!!</a>
                         @endif 
                     </p>
+                    @if($r->show($record->from->auth, 'vip'))
+                    <span class="badge badge-dark">{{ $r->show($a->from->auth, 'vip') }}VIP, 可用¥:{{ $a->sum('pay') }}</span><br>
+                    @endif
                     客户: {{ $r->show($record->consumer->info, 'name') }} {{ $r->show($record->consumer->accounts, 'mobile') }}<br>
                     地址: {{ $r->show($record->from->info, 'addr') }} <br>
                     金额: ¥{{ $record->pay }} <br>
