6130168974e17c1bfdcf096e7f6db230d854f62d
kris <kris@viirose.com>
2019-04-10 12:53:22 +0800

    init
    init

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
             <img class="ok" src="{{ URL::asset('svg/logo.svg') }}">
             <h5>众乐速配</h5>
             <p class="text text-danger">高能 . 高效</p>
+            <p>欢迎关注众乐速配-江和自补液。江和携手经销商，助您2019生意腾飞!</p>
             <p>
                 @if(strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') && Cache::has(session('openid')))
                     <a href="/register" class="btn btn-block btn-success">继续, 完成注册</a>
