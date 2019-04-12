ce6c38ad2d95c22c2e919f3620bc70dda02ed9ee
kris <kris@viirose.com>
2019-04-12 12:49:11 +0800

    init
    init

--- app/Jobs/WechatTemplet.php
+++ app/Jobs/WechatTemplet.php
@@ -38,7 +38,7 @@ class WechatTemplet implements ShouldQueue
      *
      * @return void
      */
-    public function handle(Role $role)
+    public function handle()
     {
         Log::info('handle');
 
@@ -46,35 +46,35 @@ class WechatTemplet implements ShouldQueue
 
         // $order->to->users()->where('auth->master', true)->get(); # 5.7版本不支持
 
-        // 接收机构管理员
-        foreach ($this->order->to->users as $user) {
-            if($role->master($user->id) && $role->show($user->accounts, 'openid')) array_push($openids, $role->show($user->accounts, 'openid'));
-        }
+        // // 接收机构管理员
+        // foreach ($this->order->to->users as $user) {
+        //     if($role->master($user->id) && $role->show($user->accounts, 'openid')) array_push($openids, $role->show($user->accounts, 'openid'));
+        // }
 
-        if($role->show($this->order->from->orgMan->accounts, 'openid')) array_push($openids, $role->show($this->order->from->orgMan->accounts, 'openid'));
+        // if($role->show($this->order->from->orgMan->accounts, 'openid')) array_push($openids, $role->show($this->order->from->orgMan->accounts, 'openid'));
 
-        array_unique($openid);
-        Log::info($openid);
+        // array_unique($openid);
+        // Log::info($openid);
 
-        if(count($openids)) {
-            foreach ($openids as $openid) {
-                $array = [
-                    'openid' => $openid,
-                    'template_id' => config('wechat.templets.order_new'),
-                    'url' => URL::asset('/orders/show/'.$this->order->from->id),
-                    'first' => '新订单通知',
-                    'remark' => '请及时与订货人联系确认',
-                    'keywords' => [
-                        $this->order->pay, # '订单金额',
-                        $this->order->goods->name . $this->order->goods->type .'×'. $this->order->num, #'订单详情',
-                        $this->order->id, # '订单号',
-                        $this->order->from->name . $role->show($this->order->from_user->accounts, 'mobile'), # '买家会员',
-                    ],
-                ];
-                // 发送
-                Log::warning($array);
-            }  
-        }
+        // if(count($openids)) {
+        //     foreach ($openids as $openid) {
+        //         $array = [
+        //             'openid' => $openid,
+        //             'template_id' => config('wechat.templets.order_new'),
+        //             'url' => URL::asset('/orders/show/'.$this->order->from->id),
+        //             'first' => '新订单通知',
+        //             'remark' => '请及时与订货人联系确认',
+        //             'keywords' => [
+        //                 $this->order->pay, # '订单金额',
+        //                 $this->order->goods->name . $this->order->goods->type .'×'. $this->order->num, #'订单详情',
+        //                 $this->order->id, # '订单号',
+        //                 $this->order->from->name . $role->show($this->order->from_user->accounts, 'mobile'), # '买家会员',
+        //             ],
+        //         ];
+        //         // 发送
+        //         Log::warning($array);
+        //     }  
+        // }
 
 
     }
