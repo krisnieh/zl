4183d2173d3e4cccc0afb2adfcc2f356b10e2fb4
kris <kris@viirose.com>
2019-04-12 13:02:48 +0800

    init
    init

--- app/Jobs/WechatOrderNew.php
+++ app/Jobs/WechatOrderNew.php
@@ -11,6 +11,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Log;
 
 use App\Order;
+use App\Helpers\Role;
 
 class WechatOrderNew implements ShouldQueue
 {
@@ -34,8 +35,41 @@ class WechatOrderNew implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(Role, $role)
     {
-        Log::info('handle');
+        $openids = [];
+        // Log::info('handle');
+
+        // $this->order->to->users()->where('auth->master', true)->get(); # 5.7版本不支持
+
+        // 接收机构管理员
+        foreach ($this->order->to->users as $user) {
+            if($role->master($user->id) && $role->show($user->accounts, 'openid')) array_push($openids, $role->show($user->accounts, 'openid'));
+        }
+
+        if($role->show($this->order->from->orgMan->accounts, 'openid')) array_push($openids, $role->show($this->order->from->orgMan->accounts, 'openid'));
+
+        array_unique($openid);
+        Log::info($openid);
+
+        if(count($openids)) {
+            foreach ($openids as $openid) {
+                $array = [
+                    'openid' => $openid,
+                    'template_id' => config('wechat.templets.order_new'),
+                    'url' => URL::asset('/orders/show/'.$this->order->from->id),
+                    'first' => '新订单通知',
+                    'remark' => '请及时与订货人联系确认',
+                    'keywords' => [
+                        $this->order->pay, # '订单金额',
+                        $this->order->goods->name . $this->order->goods->type .'×'. $this->order->num, #'订单详情',
+                        $this->order->id, # '订单号',
+                        $this->order->from->name . $role->show($this->order->from_user->accounts, 'mobile'), # '买家会员',
+                    ],
+                ];
+                // 发送
+                Log::warning($array);
+            }  
+        }
     }
 }
